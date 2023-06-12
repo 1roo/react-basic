56092590c73949f461c4d22a89ca7b6ac821c9cc
1roo <93hanaru@gmail.com>
2023-06-12 15:49:02 +0900

    2. 이벤트와 스타일

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import SayHello from './SayHello';
 import Hello from './Hello';
+import SubmitEvent from './component/chap01_event/SubmitEvent';
 
 function App() {
   // return (
@@ -33,7 +34,7 @@ function App() {
     {} 안에서 함수의 호출문이나 변수참조는 가능*/
   return (
     <>
-       { looping() }
+      <SubmitEvent />
     </>
   );
 }
