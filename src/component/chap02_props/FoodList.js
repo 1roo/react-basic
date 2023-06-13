e4c68dcc75dff1c91178e623935485b5dc93f98b
1roo <93hanaru@gmail.com>
2023-06-13 10:15:01 +0900

    2. props활용

--- src/component/chap02_props/FoodList.js
+++ src/component/chap02_props/FoodList.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import FoodItem from './FoodItem'
+import React from 'react';
+import FoodItem from './FoodItem';
+import SayHEllo from '../../SayHello';
 
 const FoodList = () => {
     /* fetch를 통해 서버에서 음식 목록을 json으로 받아왔을 때 */
@@ -42,6 +43,14 @@ const FoodList = () => {
         {/*<FoodItem foodName='자장면' price={6000} foodId='chinese' />
         <FoodItem foodName='해물파전' price={12000} foodId='korean' />
         <FoodItem foodName='가츠동' price={8000} foodId='japanese' />*/}
+
+        <SayHEllo>
+            <a href='https://www.naver.com'>네이버 링크</a>
+        </SayHEllo>    
+        <SayHEllo>
+            <a href='https://www.google.com'>구글 링크</a>
+        </SayHEllo>    
+
         {
             food.foodList.map(f => <FoodItem foodName={f.fName} price={f.price} foodId={f.id} />)
         }
